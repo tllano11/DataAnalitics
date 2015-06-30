0dfebe9adf41aecd5ca72d25afd7205119bba618
Gustavo Gutierrez <gustavo.ggutierrez@gmail.com>
2015-06-30 10:14:59 -0500

    Number of clusters greater than 2

--- findFacebookK.m
+++ findFacebookK.m
@@ -1,6 +1,7 @@
 function [V] = findFacebookK(G,clusters)
-  V = [];;
-  for i = 1:clusters
+  V = [];
+  % Has to be from 2 otherwise kmeans rises an error
+  for i = 2:clusters
     [idx, c, m] = facebook(G,i);
     V = [V [i; m]];
     disp(['clusters ', num2str(i), ' metric: ', num2str(m)]);
