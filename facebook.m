cc01caeb704dfb09b55c38e4819eee036e54a3e6
Gustavo Gutierrez <gustavo.ggutierrez@gmail.com>
2015-06-30 10:06:38 -0500

    Gathering stats

--- facebook.m
+++ facebook.m
@@ -1,18 +1,6 @@
 function [idx,c,M] = facebook(G, clusters) 
-  [idx,c] = k_means(G,clusters,'EmptyAction','singleton');
-  M = 0;
-  for j = 1:clusters
-    cluster = find(idx==j);
-    rows = G(cluster);
-    center = c(j,:);
-    for i = 1:length(cluster)
-      point = rows(i,:);
-      distance = sum((point - center).*(point - center));
-      %v = point - center;
-      %distance = (v * v')^2; %norm(point -  center);
-      M = M + distance;   
-    end
-  end
+  [idx,c,sumD] = k_means(G,clusters,'EmptyAction','singleton');
+  M = sum(sumD);
   disp(['Metric ', num2str(M)]);
   clusterStats(idx);
 end
