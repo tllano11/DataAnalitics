9839389b7323b765c6542c4e271fde9288d5fc40
Gustavo Gutierrez <gustavo.ggutierrez@gmail.com>
2015-06-30 14:48:20 -0500

    Better indent

--- decompose.m
+++ decompose.m
@@ -2,15 +2,15 @@ function [Decomp] = decompose(W, k)
 %   'W' - Adjacency matrix, needs to be square
 %   'k' - Number of eigen vectors in the decomposition
 
-% calculate degree matrix
-degs = sum(W, 2);
-D    = sparse(1:size(W, 1), 1:size(W, 2), degs);
+    % calculate degree matrix
+    degs = sum(W, 2);
+    D    = sparse(1:size(W, 1), 1:size(W, 2), degs);
 
-% compute unnormalized Laplacian
-L = D - W;
+    % compute unnormalized Laplacian
+    L = D - W;
 
-% compute the eigenvectors corresponding to the k smallest
-% eigenvalues
-diff   = eps;
-[Decomp, ~] = eigs(L, k, diff);
+    % compute the eigenvectors corresponding to the k smallest
+    % eigenvalues
+    diff   = eps;
+    [Decomp, ~] = eigs(L, k, diff);
 end
