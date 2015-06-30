357c4da6e89745ca8d663e820842050851d321c3
Gustavo Gutierrez <gustavo.ggutierrez@gmail.com>
2015-06-30 14:51:22 -0500

    Spectral clustering for facebook

--- facebookSpec.m
+++
@@ -1,6 +0,0 @@
-function [idx,M] = facebookSpec(G, clusters) 
-  [idx,L, U,sumD] = SpectralClustering(G,clusters,1);
-  M = sum(sumD);
-  disp(['Metric ', num2str(M)]);
-  clusterStats(idx);
-end
--- findFacebookKSpec.m
+++ findFacebookKSpec.m
@@ -1,8 +1,14 @@
 function [V] = findFacebookKSpec(G,clusters)
-  V = [];
+  
+  V = []; % Stores the metrics for all the clusterings.
+
+  % Find the matrix decomposition of G 
+  [Decomp] = decompose(G,clusters); 
   % Has to be from 2 otherwise kmeans rises an error 
   for i = 2:clusters
-    [idx, m] = facebookSpec(G,i);
+  	% Run kmeans for increasing values of k on the decomposition
+  	[idx, c, sumd] = k_means(Decomp,i);
+  	m = sum(sumd);
     V = [V [i; m]];
     disp(['clusters ', num2str(i), ' metric: ', num2str(m)]);
   end
