f4419e3db777cf8a670eac321aab06d777e30813
tllanos <tllanos@eafit.edu.co>
2015-07-02 11:57:56 -0500

    findFacebookKSpec now determines which is the best k value

--- findFacebookKSpec.m
+++ findFacebookKSpec.m
@@ -1,17 +1,27 @@
-function [V,D] = findFacebookKSpec(G,clusters)
-  
-  V = []; % Stores the metrics for all the clusterings.
+% G -> Graph's Adjacency matrix
+% clusters -> Number of clusters
+function [V,D,C,Bk] = findFacebookKSpec(G,clusters)
 
-  % Find the matrix decomposition of G 
+  V = []; % Stores the metrics for all the clusterings.
+  C{1} = []; %Stores an adjacency matrix, returned by plotClustering, for each clustering
+  Bk = [0,Inf]; %Stores the best k value based on the metric
+
+  % Find the matrix decomposition of G
   [Decomp] = decompose(G,clusters);
-  D = Decomp; 
-  % Has to be from 2 otherwise kmeans rises an error 
+  D = Decomp;
+  % Has to be from 2 otherwise kmeans rises an error
   for i = 2:clusters
-  	% Run kmeans for increasing values of k on the decomposition
-  	[idx, c, sumd] = k_means(Decomp,i);
-  	m = sum(sumd);
-    V = [V [i; m]];
+    % Run kmeans for increasing values of k on the decomposition
+    [idx, c, sumd] = k_means(Decomp,i);
+    m = sum(sumd);
+    modul = modularity (idx, G);
+    if m < Bk(1,2)
+      Bk(1,1) = i;
+      Bk(1,2) = m;
+    end
+    V = [V [i; m; modul]];
+    C{i} = plotClustering(G, idx);
     disp(['clusters ', num2str(i), ' metric: ', num2str(m)]);
   end
   V = V';
-end
+  end
