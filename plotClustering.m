0febcdac8e8e462ac2e8e9bb35b16f564354c3ab
Gustavo Gutierrez <gustavo.ggutierrez@gmail.com>
2015-07-02 09:38:00 -0500

    Different color for non clustered edges

--- plotClustering.m
+++ plotClustering.m
@@ -1,4 +1,11 @@
-function [C] = plotClustering(Adj, clustering, outputFile)
+function [C] = plotClustering(Adj, clustering)
+  % Returns a classification of the edges in Adj according to their source and 
+  % target vertices. If both vertices of an edge are in the same cluster then 
+  % the returned matrix contains the cluster index in the representation of that
+  % edge.
+
+  numberOfClusters = length(unique(clustering));
+  extraCluster = numberOfClusters + 1;
   C = Adj;
   [i,j,v] = find(Adj);  % set i and j to be the indices of non-zeros in A
   pairs = [i,j];
@@ -7,6 +14,8 @@ function [C] = plotClustering(Adj, clustering, outputFile)
   	k = pairs(i,2);
   	if clustering(j) == clustering(k)
   		C(j,k) = clustering(k);
+    else
+      C(j,k) = extraCluster;
   	end
   end
 
